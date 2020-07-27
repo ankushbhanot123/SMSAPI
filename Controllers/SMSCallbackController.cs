53c62a0182717317393f7a74b457767c47b19e1a
Ankush Bhanot <ankush.passion@gmail.com>
2020-07-27 22:56:50 +0530

    Add project files.

---
+++ Controllers/SMSCallbackController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TruckAppAPISolution.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TruckAppAPISolution.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SMSCallbackController : ControllerBase
+    {
+        private readonly SMSAPIDBContext _context;
+
+        public SMSCallbackController(SMSAPIDBContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<SMSCallbackController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SMSCallbackController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/SMSCallback
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<SMSCallback>> Post([Bind("request_id,recipient_id,dest_addr,Status")]SMSCallback smsCallback)
+        {
+            _context.smsCallback.Add(smsCallback);
+            await _context.SaveChangesAsync();
+
+            
+            return new OkObjectResult(smsCallback);
+        }
+
+        // PUT api/<SMSCallbackController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SMSCallbackController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Controllers/SMSController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+//using Newtonsoft.Json;
+using SMSAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SMSAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SMSController : ControllerBase
+    {
+        private const string URL = "https://apisms.bongolive.africa/v1/send";
+        //private const string DATA = @"{""object"":{""name"":""Name""}}";
+
+        // GET: api/<SMSController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SMSController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SMSController>
+        [HttpPost]
+        public ActionResult Post([FromBody] SMSInput value)
+        {
+
+            string DATA = JsonSerializer.Serialize(value);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = DATA.Length;
+            StreamWriter requestWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII);
+            requestWriter.Write(DATA);
+            requestWriter.Close();
+
+            try
+            {
+                request.PreAuthenticate = true;
+                String username = "06c76e11ca598786";
+                String password = "ZTM4ZDRkYzk3NjZkMjdjMGNhNmRhMmE4NmU3NGE3OGNkN2I0ZGNjY2MzNGU3MmI5ZmJiNmVjMDcxOGQxZDI2MA==";
+                String encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
+                request.Headers.Add("Authorization", "Basic " + encoded);
+
+                WebResponse webResponse = request.GetResponse();
+                Stream webStream = webResponse.GetResponseStream();
+                StreamReader responseReader = new StreamReader(webStream);
+                string response = responseReader.ReadToEnd();
+                Console.Out.WriteLine(response);
+                responseReader.Close();
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("-----------------");
+                Console.Out.WriteLine(e.Message);
+                return NotFound();
+            }
+        }
+
+        // PUT api/<SMSController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SMSController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
